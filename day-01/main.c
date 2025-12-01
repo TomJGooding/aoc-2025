a79d4426da0a7bbaf35bf5bf84953d9990a6d947
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-12-01 17:26:28 +0000

    add solution for day 01 part 1

---
+++ day-01/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Missing input file argument\n");
+        exit(1);
+    }
+
+    const char *fname = argv[1];
+    FILE *f = fopen(fname, "r");
+    if (f == NULL) {
+        fprintf(stderr, "Cannot open '%s'\n", fname);
+        exit(1);
+    }
+
+    const int steps = 100;
+    int pos = 50;
+    int count = 0;
+
+    char rot;
+    int dist;
+    while (fscanf(f, " %c%d", &rot, &dist) == 2) {
+        switch (rot) {
+            case 'R':
+                pos = (pos + dist) % steps;
+                break;
+            case 'L':
+                pos = (((pos - dist) % steps) + steps) % steps;
+                break;
+        }
+        if (pos == 0) {
+            count++;
+        }
+    }
+
+    printf("--- Day 1: Secret Entrance ---\n");
+    printf("Answer for part 1: %d\n", count);
+
+    fclose(f);
+}
