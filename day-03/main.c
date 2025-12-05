ad1d8dd6fa73a4188a1e32c02e32d8c6594798dc
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-12-05 12:29:17 +0000

    add solution for day 03 part 2

--- day-03/main.c
+++ day-03/main.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
-int max_joltage(const char *bank) {
+long max_joltage(const char *bank, size_t bats) {
+    long res = 0;
+
     size_t bank_len = strlen(bank);
 
-    int d1_max = 0;
-    size_t d1_idx = 0;
-    for (size_t i = 0; i < bank_len - 1; i++) {
-        int digit = bank[i] - '0';
-        if (digit > d1_max) {
-            d1_max = digit;
-            d1_idx = i;
+    assert(bats > 0);
+    assert(bats <= bank_len);
+
+    int max = 0;
+    int max_idx = 0;
+    for (size_t bat_n = 1; bat_n <= bats; bat_n++) {
+        size_t bats_rem = bats - bat_n;
+        for (size_t i = max_idx; i < bank_len - bats_rem; i++) {
+            int digit = bank[i] - '0';
+            if (digit > max) {
+                max = digit;
+                max_idx = i;
+            }
         }
+        res = res * 10 + max;
+        max = 0;
+        max_idx += 1;
     }
 
-    int d2_max = 0;
-    for (size_t i = d1_idx + 1; i < bank_len; i++) {
-        int digit = bank[i] - '0';
-        if (digit > d2_max) {
-            d2_max = digit;
-        }
-    }
-
-    return (d1_max * 10) + d2_max;
+    return res;
 }
 
 int main(int argc, char *argv[]) {
@@ -39,16 +43,19 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    int total = 0;
+    long total_p1 = 0;
+    long total_p2 = 0;
 
     char bank[128];
     while (fgets(bank, sizeof(bank), f)) {
         bank[strlen(bank) - 1] = 0; // Remove newline
-        total += max_joltage(bank);
+        total_p1 += max_joltage(bank, 2);
+        total_p2 += max_joltage(bank, 12);
     }
 
     printf("--- Day 3: Lobby ---\n");
-    printf("Answer for part 1: %d\n", total);
+    printf("Answer for part 1: %ld\n", total_p1);
+    printf("Answer for part 2: %ld\n", total_p2);
 
     fclose(f);
 }
