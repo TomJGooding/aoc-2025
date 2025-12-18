ba12ae1707f2bea6122d9f135b8f7a363a75a7f4
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2025-12-18 17:23:10 +0000

    add solution for day 07 part 2

--- day-07/main.c
+++ day-07/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+
+#define MAX_LEN 256
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -16,33 +17,38 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    // TODO: A bitset would be more efficient!
-    bool beams[256] = {0};
+    long beams[MAX_LEN] = {0};
 
     int splits = 0;
 
-    char line[256];
+    char line[MAX_LEN];
     while (fgets(line, sizeof(line), f)) {
         for (size_t i = 0; line[i]; i++) {
             if (line[i] == 'S') {
-                beams[i] = true;
+                beams[i] = 1;
                 break;
             }
             if (line[i] == '^' && beams[i]) {
-                beams[i] = false;
                 if (i > 0) {
-                    beams[i - 1] = true;
+                    beams[i - 1] += beams[i];
                 }
                 if (i < strlen(line)) {
-                    beams[i + 1] = true;
+                    beams[i + 1] += beams[i];
                 }
+                beams[i] = 0;
                 splits++;
             }
         }
     }
 
+    long timelines = 0;
+    for (size_t i = 0; i < MAX_LEN; i++) {
+        timelines += beams[i];
+    }
+
     printf("--- Day 7: Laboratories ---\n");
     printf("Answer for part 1: %d\n", splits);
+    printf("Answer for part 2: %ld\n", timelines);
 
     fclose(f);
 }
